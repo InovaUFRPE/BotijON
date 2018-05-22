01f49b9951fb78e690cbea1b115c5c3e58c96f6f
Keyson Raphael <keyson.rapha@gmail.com>
2018-05-22 00:43:06 -0300

    Criação de pagina Home e Cadastro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CadastroPage } from '../cadastro/cadastro';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  cadastroPage = CadastroPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams){}
 
-  }
-
+    goToTabsPage(){
+      this.navCtrl.push(CadastroPage)
+    }
 }
