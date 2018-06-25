b8ccd7f2732ccae7dc320cea24310f1fc4d5ea26
KeysonRaphael <keyson.rapha@gmail.com>
2018-06-25 17:02:43 -0300

    add pages + pedido page edits

---
+++ src/pages/vendedor-gas/vendedor-gas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VendedorGasPage } from './vendedor-gas';
+
+@NgModule({
+  declarations: [
+    VendedorGasPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(VendedorGasPage),
+  ],
+})
+export class VendedorGasPageModule {}
